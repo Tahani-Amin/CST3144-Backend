3da66fe0749ca11b8ad2892dadb5dae096080a6e
Tahani-Amin <tahaniamin8@gmail.com>
2025-11-25 23:56:43 +0400

    Added env & gitignore

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var path = require("path");
 var fs = require("fs");
 
+require("dotenv").config();  
+
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
 
@@ -10,7 +12,7 @@ let db;
 
 app.use(express.json());
 
-app.set("port", 3000);
+app.set("port", process.env.PORT || 3000);   
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -23,23 +25,19 @@ app.use((req, res, next) => {
     next();
 });
 
-MongoClient.connect(
-    "mongodb+srv://tahani:tahani@cluster0.hfvhr.mongodb.net/",
-    (err, client) => {
-        if (err) {
-            console.error("Failed to connect to MongoDB", err);
-            return;
-        }
-        db = client.db("webstore");
-        console.log("Connected to MongoDB successfully! :)");
+MongoClient.connect(process.env.MONGO_URI, (err, client) => {
+    if (err) {
+        console.error("Failed to connect to MongoDB", err);
+        return;
     }
-);
-
+    db = client.db("webstore");
+    console.log("Connected to MongoDB successfully! :)");
+});
 
 app.use(function(req, res, next) {
-    console.log("Request IP: "+ req.baseUrl);
-    console.log("Request date: "+ new Date());
-    console.log("Request method: "+ req.method);
+    console.log("Request IP: " + req.baseUrl);
+    console.log("Request date: " + new Date());
+    console.log("Request method: " + req.method);
     next();
 });
 
@@ -47,26 +45,23 @@ app.get("/", function(req, res) {
     res.send("Backend server is working");
 });
 
-app.use(function(req,res,next){
-    var filepath = path.join(__dirname,"static",req.url);
-    fs.stat(filepath,function(err,fileInfo){
-        if(err){
+app.use(function(req, res, next) {
+    var filepath = path.join(__dirname, "static", req.url);
+    fs.stat(filepath, function(err, fileInfo) {
+        if (err) {
             next();
             return;
         }
-        if(fileInfo.isFile())
-            res.sendFile(filepath);
-        else
-            next();
+        if (fileInfo.isFile()) res.sendFile(filepath);
+        else next();
     });
 });
-app.use(function(req,res){
+
+app.use(function(req, res) {
     res.status(404);
     res.end("File not found!");
 });
-app.listen(3000,function(){
-    console.log("Server is running on port 3000");
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port " + (process.env.PORT || 3000));
 });
-
-
-
